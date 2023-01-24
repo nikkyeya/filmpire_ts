077ab600792ae327d382619859f544d41905f9ef
nikkyeya <nikkyeya@gmail.com>
2023-01-25 04:50:37 +0800

    Add profile page

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
+import { Typography, Button, Box } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
-import useStyles from './Profile.styles';
 import { userSelector } from '../../features/auth';
+import { useGetListQuery } from '../../services/TMDB';
+import RatedCards from '../RatedCards/RatedCards';
 
 const Profile = () => {
-  const classes = useStyles();
   const { user } = useSelector(userSelector);
 
+  const { data: favoriteMovies, refetch: refetchFavorites } = useGetListQuery({
+    listName: 'favorite/movies',
+    accountId: user.id,
+    sessionId: localStorage.getItem('session_id'),
+    page: 1,
+  });
+  const { data: watchlistMovies, refetch: refetchWatchlist } = useGetListQuery({
+    listName: 'watchlist/movies',
+    accountId: user.id,
+    sessionId: localStorage.getItem('session_id'),
+    page: 1,
+  });
+
+  useEffect(() => {
+    refetchFavorites();
+    refetchWatchlist();
+  }, []);
+
+  const logout = () => {
+    localStorage.clear();
+    window.location.href = '/';
+  };
+
   return (
-    <div className={classes.container}>
-      Profile: {user.name} ({user.id})
-    </div>
+    <Box>
+      <Box display="flex" justifyContent="space-between">
+        <Typography variant="h4" gutterBottom>
+          My Profile
+        </Typography>
+        <Button color="inherit" onClick={logout}>
+          Logout &nbsp; <ExitToApp />
+        </Button>
+      </Box>
+      {!favoriteMovies?.results?.length && !watchlistMovies?.results?.length ? (
+        <Typography variant="h5">
+          Add favorites or watchlist some movies to see them here!
+        </Typography>
+      ) : (
+        <Box>
+          <RatedCards title="Favorite Movies" data={favoriteMovies} />
+          <RatedCards title="Watchlist" data={watchlistMovies} />
+        </Box>
+      )}
+    </Box>
   );
 };
 
